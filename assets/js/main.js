b3a8a4b5d4338a268addf1e7ac01db6304c4e857
Iusto <iusto@naver.com>
2022-11-16 11:56:20 +0900

    12

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Arcana by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -39,7 +33,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">' + $('<h1><a href="main.html">(주)제이에스에프</h1>').html() + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
